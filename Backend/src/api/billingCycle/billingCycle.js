8dbbc71905c7aef44257c58e377d41e3da77bd4d
Andre Paliao <andre_paliao@hotmail.com>
2017-12-19 22:13:03 -0200

    Errors Handlers

--- Backend/src/api/billingCycle/billingCycle.js
+++ Backend/src/api/billingCycle/billingCycle.js
@@ -2,22 +2,22 @@ const restful = require('node-restful')
 const mongoose = restful.mongoose
 
 const creditSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  value: { type: Number, min: 0, required: true }
+    name: { type: String, required: true },
+    value: { type: Number, min: 0, required: [true, 'Enter the amount of the credit!'] }
 })
 
 const debtSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  value: { type: Number, min: 0, required: true},
-  status: { type: String, required: false, uppercase: true, enum: ['PAGO', 'PENDENTE', 'AGENDADO']}
+    name: { type: String, required: true },
+    value: { type: Number, min: 0, required: [true, 'Enter the amount of the debt!'] },
+    status: { type: String, required: false, uppercase: true, enum: ['PAYED', 'PENDING', 'SCHEDULED'] }
 })
 
 const billingCycleSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  month: { type: Number, min: 1, max: 12, required: true },
-  year: { type: Number, min:1965, max: 2100, required: true},
-  credits: [creditSchema], 
-  debts: [debtSchema]
+    name: { type: String, required: true },
+    month: { type: Number, min: 1, max: 12, required: true },
+    year: { type: Number, min: 1965, max: 2100, required: true },
+    credits: [creditSchema],
+    debts: [debtSchema]
 })
 
 module.exports = restful.model('BillingCycle', billingCycleSchema)
--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -2,3 +2,12 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
 module.exports = mongoose.connect('mongodb://localhost/mymoney')
+
+
+mongoose.Error.messages.general.required = "The attributte '{PATH}' is required."
+mongoose.Error.messages.Number.min = 
+    "The '{VALUE}' informed is lesser than the minimun limit of '{MIN}'."
+mongoose.Error.messages.Number.max = 
+"The '{VALUE}' informed is lesser than the minimun limit of '{MAX}'."
+mongoose.Error.messages.String.enum = 
+    "'{VALUE}' isn't valid for the attributte '{PATH}'."
