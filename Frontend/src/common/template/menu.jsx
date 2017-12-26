434c9e742ead32d33e1cc9be7d74368f3cc62682
Andre Paliao <andre_paliao@hotmail.com>
2017-12-25 23:58:53 -0200

    Menu item created

---
+++ Frontend/src/common/template/menu.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import MenuItem from './menuItem'
+
+export default props => (
+  <ul className='sidebar-menu'>
+    <MenuItem path='#/' label='Dashboard' icon='dashboard' />
+  </ul>
+)
--- Frontend/src/main/app.jsx
+++ Frontend/src/main/app.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 import '../common/template/dependencies'
 import Header from '../common/template/header'
+import Sidebar from '../common/template/sidebar'
 
 export default props => (
   <div className='wrapper'>
-    < Header/>
+    <Header/>
+    <Sidebar />
   </div>
 )
