307130d5099d773fbd97970ff30767a00db07397
Andre Paliao <andre_paliao@hotmail.com>
2017-12-26 23:34:38 -0200

    All initial app Done

--- Frontend/src/main/app.jsx
+++ Frontend/src/main/app.jsx
@@ -3,10 +3,16 @@ import React from 'react'
 import '../common/template/dependencies'
 import Header from '../common/template/header'
 import Sidebar from '../common/template/sidebar'
+import Footer from '../common/template/footer'
+import Routes from './routes'
 
 export default props => (
   <div className='wrapper'>
-    <Header/>
+    <Header />
     <Sidebar />
+    <div className='content-wrapper'>
+      <Routes />
+    </div>
+    <Footer />
   </div>
 )
