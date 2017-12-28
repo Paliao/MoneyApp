280717af43fe4ec55f41a9452b3bd11bc333e6ea
Andre Paliao <andre_paliao@hotmail.com>
2017-12-27 23:49:30 -0200

    Initial integration of redux

--- Frontend/src/common/template/header.jsx
+++ Frontend/src/common/template/header.jsx
@@ -10,7 +10,7 @@ export default props => (
       </span>
     </a>
     <nav className='navbar navbar-static-top'>
-      <a href className='sidebar-toggle' data-toggle='offcanvas'></a>
+      <a className='sidebar-toggle' data-toggle='offcanvas'></a>
     </nav>
   </header>
 )
