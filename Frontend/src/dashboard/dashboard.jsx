0ec834084661beb8867d14648548f7a52cf7e2a8
Andre Paliao <andre_paliao@hotmail.com>
2017-12-30 23:55:13 -0200

    Creating actions for dashboard and applying middlewares

--- Frontend/src/dashboard/dashboard.jsx
+++ Frontend/src/dashboard/dashboard.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 
+import { getSummary } from './dashboardActions'
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
 import ValueBox from '../common/widget/valueBox'
 import Row from '../common/layout/row'
 
 class Dashboard extends Component {
+  componentWillMount() {
+    this.props.getSummary()
+  }
   render() {
     const { credit, debt } = this.props.summary
     return (
@@ -25,6 +30,6 @@ class Dashboard extends Component {
 }
 
 const mapStateToProps = state => ({ summary: state.dashboard.summary })
+const mapDispatchToProps = dispatch => bindActionCreators({ getSummary }, dispatch)
 
-
-export default connect(mapStateToProps)(Dashboard)
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
