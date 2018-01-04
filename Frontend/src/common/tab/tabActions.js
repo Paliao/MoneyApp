eee06387b5e3b89c3ee358ea1c5c0f0f0ce57bb9
Andre Paliao <andre_paliao@hotmail.com>
2018-01-04 18:49:56 -0200

    Show tabs added

--- Frontend/src/common/tab/tabActions.js
+++ Frontend/src/common/tab/tabActions.js
@@ -4,3 +4,13 @@ export function selectTab(tabId) {
     payload: tabId
   }
 }
+
+export function showTabs(...tabIds) {
+  const tabsToShow = {}
+  tabIds.forEach(e => tabsToShow[e] = true)
+
+  return {
+    type: 'TAB_SHOWED',
+    payload: tabsToShow
+  }
+}
