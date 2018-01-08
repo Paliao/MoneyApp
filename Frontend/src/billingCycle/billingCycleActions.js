f1180698e64e910cf09103acf6181fd904b68bec
Andre Paliao <andre_paliao@hotmail.com>
2018-01-08 03:04:27 -0200

    Errors and successfull messages inclusion

--- Frontend/src/billingCycle/billingCycleActions.js
+++ Frontend/src/billingCycle/billingCycleActions.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { toastr } from 'react-redux-toastr'
 
 const BASE_URL = 'http://localhost:3003/api'
 
@@ -12,6 +13,12 @@ export function getList() {
 
 export function create(values) {
   axios.post(`${BASE_URL}/billingCycles`, values)
+    .then( resp => {
+      toastr.success('Success', 'Creation successfully done')
+    })
+    .catch( e => {
+      e.response.data.errors.forEach(error => toastr.error('Erro', error))
+    })
   return {
     type: 'TEMP'
   }
