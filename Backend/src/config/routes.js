e073bdaa3700bf4a70139a77e49df5d138ae4fc7
Andre Paliao <andre_paliao@hotmail.com>
2018-01-15 15:31:26 -0200

    Routes done

--- Backend/src/config/routes.js
+++ Backend/src/config/routes.js
@@ -1,9 +1,21 @@
 const express = require('express')
+const auth = require('./auth')
 
 module.exports = function(server) {
-  const router = express.Router()
-  server.use('/api', router)
+  const protectedApi = express.Router()
+  server.use('/api', protectedApi)
+
+  protectedApi.use(auth)
 
   const BillingCycle = require('../api/billingCycle/biliingCycleService')
-  BillingCycle.register(router, '/billingCycles')
+  BillingCycle.register(protectedApi, '/billingCycles')
+ 
+
+  const openApi = express.Router()
+  server.use('/oapi', openApi)
+
+  const AuthService = require('../api/user/authService')
+  openApi.post('/login', AuthService.login)
+  openApi.post('/signup', AuthService.signup)
+  openApi.post('/validateToken', AuthService.validateToken)
 }
