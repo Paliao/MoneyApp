2187b0b598c1e2013aac73aa408d304e137211e6
Andre Paliao <andre_paliao@hotmail.com>
2018-01-15 13:15:13 -0200

    Permiting header through cors and token verification

--- Backend/src/config/cors.js
+++ Backend/src/config/cors.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS, PUT, PATCH, DELETE')
-  res.header('Access-Control-Allow-Headers', 'Orign, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Headers', 'Orign, X-Requested-With, Content-Type, Accept, authorization')
   next()
 }
