075ec12126b9d3bc28e5bd4a7e11c1f7ec72050b
Paliao <andre_paliao@hotmail.com>
2018-01-26 13:30:55 -0200

    Changing the verisons of mongoose due problems with cursor inside an aggregate function

--- Backend/src/api/billingCycle/biliingCycleService.js
+++ Backend/src/api/billingCycle/biliingCycleService.js
@@ -17,7 +17,7 @@ BillingCycle.route('count', (req, res, next) => {
 
 BillingCycle.route('summary', (req, res, next ) => {
   BillingCycle.aggregate({
-    $project: { credit: { $sum: "$credits.value"}, debt: { $sum: "$debts.value"}},
+    $project: { credit: { $sum: "$credits.value"}, debt: { $sum: "$debts.value"}}
   }, {
     $group: { _id: null, credit: { $sum: "$credit"}, debt: {$sum: "$debt"}}
   }, {
